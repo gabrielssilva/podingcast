a5dc3fd19d897cf6420c75d1a78edc49d51928c9
Gabriel Silva <gabriel.ssilva@outlook.com>
2014-07-30 20:07:47 -0300

    Building Podcast model

--- app/src/main/java/gabrielssilva/podingcast/model/Podcast.java
+++ app/src/main/java/gabrielssilva/podingcast/model/Podcast.java
@@ -1,4 +1,33 @@
 package gabrielssilva.podingcast.model;
 
 public class Podcast {
+
+    private String podcastName;
+    private String rssAddress;
+
+    public Podcast() {
+
+    }
+
+    public Podcast(String podcastName, String rssAddress) {
+        this.podcastName = podcastName;
+        this.rssAddress = rssAddress;
+    }
+
+
+    public String getRssAddress() {
+        return rssAddress;
+    }
+
+    public void setRssAddress(String rssAddress) {
+        this.rssAddress = rssAddress;
+    }
+
+    public String getPodcastName() {
+        return podcastName;
+    }
+
+    public void setPodcastName(String podcastName) {
+        this.podcastName = podcastName;
+    }
 }
