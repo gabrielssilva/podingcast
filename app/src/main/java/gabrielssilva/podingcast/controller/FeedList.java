1e66ee995bc01f223e54bb90ca19960a9f2f47e1
Gabriel Silva <gabriel.ssilva@outlook.com>
2014-09-10 14:59:15 -0300

    Feed List shows only .mp3 files

--- app/src/main/java/gabrielssilva/podingcast/controller/FeedList.java
+++ app/src/main/java/gabrielssilva/podingcast/controller/FeedList.java
@@ -3,9 +3,11 @@ package gabrielssilva.podingcast.controller;
 import android.os.Environment;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Filter;
 
 public class FeedList {
 
@@ -27,7 +29,12 @@ public class FeedList {
 
     public List<File> getAllFiles() {
         List<File> allFiles;
-        allFiles = Arrays.asList(this.podingcastFolder.listFiles());
+        File[] filteredFiles;
+
+        // Using our Filter, declared above.
+        FilterByExtension filter = new FilterByExtension();
+        filteredFiles = this.podingcastFolder.listFiles(filter);
+        allFiles = Arrays.asList(filteredFiles);
 
         return allFiles;
     }
@@ -36,5 +43,18 @@ public class FeedList {
         List<File> allFiles = getAllFiles();
 
         return allFiles.get(index);
+    }
+
+    /* A private class to filter our files.
+     * This class is pretty small, so declare it here should not make harm.
+     */
+    private class FilterByExtension implements FilenameFilter {
+
+        private final String ACCEPTED_EXTENSION = ".mp3";
+
+        @Override
+        public boolean accept(File file, String name) {
+            return name.toLowerCase().endsWith(this.ACCEPTED_EXTENSION);
+        }
     }
 }
